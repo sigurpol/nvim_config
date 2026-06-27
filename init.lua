b3c51be8e23e6718aff9949aed7323d46eb7a6a2
Paolo La Camera <paolo@parity.io>
2026-06-27 20:35:16 +0200

    Remove startup time in landing page

--- init.lua
+++ init.lua
@@ -1,20 +1,5 @@
-vim.g.nvim_start_ns = vim.uv.hrtime()
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
-
-local startup_group = vim.api.nvim_create_augroup("config_startup_time", { clear = true })
-local function record_startup_time()
-  if not vim.g.nvim_startup_ms then
-    vim.g.nvim_startup_ms = (vim.uv.hrtime() - vim.g.nvim_start_ns) / 1e6
-  end
-end
-
-vim.api.nvim_create_autocmd({ "UIEnter", "VimEnter" }, {
-  group = startup_group,
-  callback = function()
-    record_startup_time()
-  end,
-})
 
 if vim.fn.has("nvim-0.12") == 0 or vim.pack == nil then
   vim.notify("nvim requires Neovim 0.12+ with vim.pack", vim.log.levels.ERROR)
